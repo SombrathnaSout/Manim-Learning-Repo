5c14a0e763b90d7d32037f4e80fcf22bbf73d5ff
SombrathnaSout <138176913+SombrathnaSout@users.noreply.github.com>
2024-12-30 20:25:15 +0700

    animation core

--- helloworld.py
+++ helloworld.py
@@ -34,3 +34,41 @@ class AnimatedSquareToCircle(Scene):
         self.play(
             circle.animate.set_fill(PINK, opacity=0.5) # change the color of the circle with animation
         )
+
+class DifferentRotation(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2*LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2*RIGHT)
+
+        self.play(left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_times=2) # left square rotates 180 degrees, right square rotates 180 degrees but left square rotates inward
+        self.wait()
+
+class TwoTransform(Scene):
+    def transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(Transform(a,b))
+        self.play(Transform(b,c))
+        self.play(FadeOut(a))
+    def replacement_transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(ReplacementTransform(a,b))
+        self.play(ReplacementTransform(b,c))
+        self.play(FadeOut(a))
+    def construct(self):
+        self.transform()
+        self.wait(0.5)
+        self.replacement_transform()
+
+class TransformCycle(Scene):
+    def construct(self):
+        a = Circle()
+        t1 = Square()
+        t2 = Triangle()
+        self.add(a)
+        self.wait()
+        for t in [t1,t2]:
+            self.play(Transform(a,t))
