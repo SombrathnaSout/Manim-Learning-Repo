356883f96c3757d925e717cc50377d9b07378085
SombrathnaSout <138176913+SombrathnaSout@users.noreply.github.com>
2024-12-30 20:47:57 +0700

    update ignore

--- helloworld.py
+++ helloworld.py
@@ -43,24 +43,26 @@ class DifferentRotation(Scene):
         self.play(left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_times=2) # left square rotates 180 degrees, right square rotates 180 degrees but left square rotates inward
         self.wait()
 
-class TwoTransform(Scene):
+class TwoTransforms(Scene):
     def transform(self):
         a = Circle()
         b = Square()
         c = Triangle()
-        self.play(Transform(a,b))
-        self.play(Transform(b,c))
+        self.play(Transform(a, b))
+        self.play(Transform(a, c))
         self.play(FadeOut(a))
+
     def replacement_transform(self):
         a = Circle()
         b = Square()
         c = Triangle()
-        self.play(ReplacementTransform(a,b))
-        self.play(ReplacementTransform(b,c))
-        self.play(FadeOut(a))
+        self.play(ReplacementTransform(a, b))
+        self.play(ReplacementTransform(b, c))
+        self.play(FadeOut(c))
+
     def construct(self):
         self.transform()
-        self.wait(0.5)
+        self.wait(0.5)  # wait for 0.5 seconds
         self.replacement_transform()
 
 class TransformCycle(Scene):
